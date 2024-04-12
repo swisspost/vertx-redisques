899de8e10dc09cbd0f056e50a2e593d7accc5522
Xin Zheng <xin.zheng@post.ch>
2024-04-12 15:58:00 +0700

    improved error handling

--- src/main/java/org/swisspush/redisques/util/DequeueStatisticCollector.java
+++ src/main/java/org/swisspush/redisques/util/DequeueStatisticCollector.java
@@ -6,6 +6,7 @@ import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.shareddata.AsyncMap;
+import io.vertx.core.shareddata.Lock;
 import io.vertx.core.shareddata.SharedData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +24,11 @@ public class DequeueStatisticCollector {
     }
 
     public void setDequeueStatistic(final String queueName, final DequeueStatistic dequeueStatistic) {
-        sharedData.getLock(DEQUEUE_STATISTIC_LOCK_PREFIX.concat(queueName)).onSuccess(lock ->
+        sharedData.getLock(DEQUEUE_STATISTIC_LOCK_PREFIX.concat(queueName)).onComplete(lockAsyncResult -> {
+            if (lockAsyncResult.failed()) {
+                log.error("Failed to lock dequeue statistic data for queue {}.", queueName, lockAsyncResult.cause());
+            } else {
+                final Lock lock = lockAsyncResult.result();
                 sharedData.getAsyncMap(DEQUEUE_STATISTIC_DATA, (Handler<AsyncResult<AsyncMap<String, DequeueStatistic>>>) asyncResult -> {
                     if (asyncResult.failed()) {
                         log.error("Failed to get shared dequeue statistic data map.", asyncResult.cause());
@@ -33,50 +38,57 @@ public class DequeueStatisticCollector {
                     AsyncMap<String, DequeueStatistic> asyncMap = asyncResult.result();
                     asyncMap.size().onComplete(mapSizeResult -> {
                         log.debug("shared dequeue statistic map size: {}", mapSizeResult.result());
-                        asyncMap.get(queueName).onSuccess(sharedDequeueStatistic -> {
-                            if (sharedDequeueStatistic == null) {
-                                try {
-                                    asyncMap.put(queueName, dequeueStatistic).onSuccess(event -> {
-                                        log.debug("shared dequeue statistic for queue {} added.", queueName);
+                        asyncMap.get(queueName).onComplete(dequeueStatisticAsyncResult -> {
+                            if (dequeueStatisticAsyncResult.failed()) {
+                                lock.release();
+                                log.error("Failed to get shared dequeue statistic data for queue {}.", queueName, dequeueStatisticAsyncResult.cause());
+                            } else {
+                                final DequeueStatistic sharedDequeueStatistic = dequeueStatisticAsyncResult.result();
+                                if (sharedDequeueStatistic == null) {
+                                    try {
+                                        asyncMap.put(queueName, dequeueStatistic).onComplete(voidAsyncResult -> {
+                                            if (voidAsyncResult.failed()){
+                                                log.error("shared dequeue statistic for queue {} failed to add.", queueName);
+                                            } else {
+                                                log.debug("shared dequeue statistic for queue {} added.", queueName);
+                                            }
+                                            lock.release();
+                                        });
+                                    } catch (Exception exception) {
+                                        log.error("Failed to put shared dequeue statistic for queue {}.", queueName, exception);
                                         lock.release();
-                                    }).onFailure(event -> {
-                                        log.debug("shared dequeue statistic for queue {} failed to add.", queueName);
-                                        lock.release();
-                                    });
-                                } catch (Exception exception) {
-                                    log.error("Failed to put shared dequeue statistic for queue {}.", queueName, exception);
+                                    }
+                                } else if (sharedDequeueStatistic.getLastUpdatedTimestamp() < dequeueStatistic.getLastUpdatedTimestamp()) {
+                                    if (dequeueStatistic.isMarkedForRemoval()) {
+                                        // delete
+                                        asyncMap.remove(queueName).onComplete(removeAsyncResult -> {
+                                            if (removeAsyncResult.failed()){
+                                                log.error("failed to removed shared dequeue statistic for queue {}.", queueName);
+                                            } else {
+                                                log.debug("shared dequeue statistic for queue {} removed.", queueName);
+                                            }
+                                            lock.release();
+                                        });
+                                    } else {
+                                        // update
+                                        asyncMap.put(queueName, dequeueStatistic).onComplete(event -> {
+                                            if (event.failed()) {
+                                                log.error("shared dequeue statistic for queue {} failed to update.", queueName);
+                                            } else {
+                                                log.debug("shared dequeue statistic for queue {} updated.", queueName);
+                                            }
+                                            lock.release();
+                                        });
+                                    }
+                                } else {
+                                    log.debug("shared dequeue statistic for queue {} has newer data, update skipped", queueName);
                                     lock.release();
                                 }
-                            } else if (sharedDequeueStatistic.getLastUpdatedTimestamp() < dequeueStatistic.getLastUpdatedTimestamp()) {
-                                if (dequeueStatistic.isMarkedForRemoval()) {
-                                    // delete
-                                    asyncMap.remove(queueName).onComplete(dequeueStatisticAsyncResult -> {
-                                        log.debug("shared dequeue statistic for queue {} removed.", queueName);
-                                        lock.release();
-                                    });
-                                } else {
-                                    // update
-                                    asyncMap.put(queueName, dequeueStatistic).onComplete(event -> {
-                                        if (event.succeeded()) {
-                                            log.debug("shared dequeue statistic for queue {} updated.", queueName);
-                                        } else {
-                                            log.debug("shared dequeue statistic for queue {} failed to update.", queueName);
-                                        }
-                                        lock.release();
-                                    });
-                                }
-                            } else {
-                                log.debug("shared dequeue statistic for queue {} has newer data, update skipped", queueName);
-                                lock.release();
                             }
-                        }).onFailure(throwable -> {
-                            lock.release();
-                            log.error("Failed to get shared dequeue statistic data for queue {}.", queueName, throwable);
                         });
-
                     });
-                })).onFailure(throwable -> {
-            log.error("Failed to lock dequeue statistic data for queue {}.", queueName, throwable);
+                });
+            }
         });
     }
 
