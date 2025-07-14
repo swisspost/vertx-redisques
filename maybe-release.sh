73efab2b5463776b075fde9aeec3831fe2b99d00
Xin Zheng <xin.zheng@post.ch>
2025-07-14 11:29:34 +0700

    try to release with plugin

--- maybe-release.sh
+++ maybe-release.sh
@@ -2,32 +2,13 @@
 set -ev
 git fetch
 git reset --hard
-groovy staging.groovy drop
-rc=$?
-if [ $rc -ne 0 ]
-then
-  echo 'problem when trying to drop, ignored'
-fi
-echo 'starting a new nexus repository ...'
-OUTPUT=$(groovy staging.groovy start)
-echo "repository Id: $OUTPUT"
-mvn -B -Prelease jgitflow:release-start jgitflow:release-finish -DrepositoryId=${OUTPUT}
+mvn -B -Prelease gitflow:release-start -DskipTests
+mvn -B -Prelease --batch-mode deploy -DskipTests
+mvn -B -Prelease gitflow:release-finish -DskipTests
 rc=$?
 if [ $rc -eq 0 ]
 then
-    groovy staging.groovy close ${OUTPUT}
-    groovy staging.groovy promote ${OUTPUT}
-    rc=$?
-    if [ $rc -ne 0 ]
-    then
-      echo 'Release failed, cannot promote stage'
-      exit $rc
-    fi
     echo 'Release done, will push'
-    git tag
-    git push --tags
-    git checkout develop
-    git push origin develop
   exit 0
 fi
 echo 'Release failed'
