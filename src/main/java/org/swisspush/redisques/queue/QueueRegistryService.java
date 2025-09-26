758d799b973262e47cc76902edf1266709956f99
Xin Zheng <xin.zheng@post.ch>
2025-09-26 14:00:49 +0700

    fixed typo

--- src/main/java/org/swisspush/redisques/queue/QueueRegistryService.java
+++ src/main/java/org/swisspush/redisques/queue/QueueRegistryService.java
@@ -691,7 +691,7 @@ public class QueueRegistryService {
                         log.warn("Failed to removed consumer '{}'", key, exceptionFactory.newException(result.cause()));
                     } else {
                         log.debug("{} consumer key removed", result.result().toLong());
-                        // need fin a new consumer for this queue, let's make a peer become consumer
+                        // need find a new consumer for this queue, let's make a peer become consumer
                         log.debug("RedisQues Sending new registration request for queue {}", queueName);
                         eb.send(keyspaceHelper.getConsumersAddress(), queueName);
                     }
